93b8a61960816e04c458263afccd3d7c9e257aab
PAVANSAIREDDY <44047375+PAVANSAIREDDY@users.noreply.github.com>
2022-10-29 12:12:28 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -17,3 +17,8 @@ def newtest()
 {
   sh "java -jar /home/ubuntu/.jenkins/workspace/sharedlibraries/testing.jar"
 }
+
+def newdel(job,ip,context)
+{
+  sh "scp /home/ubuntu/.jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
